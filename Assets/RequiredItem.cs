69c073b6de82fc67a8e03d63eaff317e19ed3c41
Egrez <35452979+Egrez@users.noreply.github.com>
2018-01-28 18:27:52 +0800

    12321

---
+++ Assets/RequiredItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequiredItem : MonoBehaviour {
+	
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("Player")){
+			public bool QuestCompleted = true
+			Destroy(RequiredItem)
+		}	
+	}
+
+
+}
